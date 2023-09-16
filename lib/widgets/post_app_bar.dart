85d38d7d260a6d84a2127417639c532598e69182
Kunal Hemani <kunalnhemani@gmail.com>
2023-09-16 21:22:24 +0530

    Travel App Demo in flutter

--- lib/widgets/post_app_bar.dart
+++ lib/widgets/post_app_bar.dart
@@ -6,54 +6,56 @@ class PostAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Padding(
-        padding: EdgeInsets.all(20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            InkWell(
-              onTap: (){
-                Navigator.pop(context);
-              },
-              child: Container(
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.white,
-                      blurRadius: 5,
-                    ),
-                  ]
-                ),
-                child: Icon(
-                  Icons.arrow_back_ios_sharp,
-                  size: 28,
-                ),
-              ),
-            ),
-            InkWell(
-              onTap: (){},
-              child: Container(
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                    color: Colors.white,
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              InkWell(
+                onTap: (){
+                  Navigator.pop(context);
+                },
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(15),
+                    color: Colors.white,
                     boxShadow: [
                       BoxShadow(
                         color: Colors.white,
-                        blurRadius: 3,
+                        blurRadius: 1,
                       ),
                     ]
-                ),
-                child: Icon(
-                  Icons.favorite, color: Colors.redAccent,
-                  size: 28,
+                  ),
+                  child: Icon(
+                    Icons.arrow_back_ios_sharp,
+                    size: 28,
+                  ),
                 ),
               ),
-              ),
-          ],
+              InkWell(
+                onTap: (){},
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.white,
+                          blurRadius: 1,
+                        ),
+                      ]
+                  ),
+                  child: Icon(
+                    Icons.favorite, color: Colors.redAccent,
+                    size: 28,
+                  ),
+                ),
+                ),
+            ],
+          ),
         ),
       ),
     );
